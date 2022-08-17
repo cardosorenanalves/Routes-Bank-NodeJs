3112fe6716a40ded0f10a744335ff5ea4c7f0120
Renan Cardoso <renancardoso0307@gmail.com>
2022-08-16 22:39:36 -0300

    inicializando projeto

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const { response } = require('express');
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+
+const app = express();
+
+app.use(express.json())
+
+const customers = []
+
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
+
+    const costumerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+         );
+
+    if(costumerAlreadyExists){
+        return response.status(400).json({ error: "Customer already exists!" })
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id : uuidv4(),
+        statemennt: []
+    })
+
+    return response.status(201).send()
+
+})
+
+app.get( '/statement', (request, response )=> {
+    const {cpf} = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer){
+        return response.status(400).json({error: "Customer not found!"})
+    }
+
+    return response.json(customer.statement)
+})
+
+app.listen(3333);
