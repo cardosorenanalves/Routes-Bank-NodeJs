510271a1da22a744dce9b1729ff5af8c833d5f4d
Renan Cardoso <renancardoso0307@gmail.com>
2022-08-19 14:04:03 -0300

    Routes: delete.account create

--- src/index.js
+++ src/index.js
@@ -134,4 +134,12 @@ app.get( '/account', verifyIfExistAccountCPF, (request, response )=> {
     return response.json(customer);
 });
 
+app.delete('/account', verifyIfExistAccountCPF, (request, response)=> {
+    const {customer} = request;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
 app.listen(3333);
